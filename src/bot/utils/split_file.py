e50c3096eb695ff1c6c0f78908486321720854b7
alambertt <github.vtddi@zico.slmail.me>
2023-11-23 13:07:42 -0500

    Refactor split_file function to export parts with sequential part numbers

--- src/bot/utils/split_file.py
+++ src/bot/utils/split_file.py
@@ -8,12 +8,14 @@ def split_file(file_path: str, duration_millis=MAX_AUDIO_DURATION) -> list[str]:
     try:
         audio = AudioSegment.from_file(file_path)
         parts = []
+        part_index = 1
 
         for i in range(0, len(audio), duration_millis):
             part = audio[i : i + duration_millis]
-            part_file_path = f"{file_path}_part{i}.mp3"
+            part_file_path = f"{file_path}_part{part_index}.mp3"
             part.export(part_file_path, format="mp3")
             parts.append(part_file_path)
+            part_index += 1
 
         return parts
     except Exception as e:
